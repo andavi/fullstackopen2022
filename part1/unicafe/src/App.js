7e281242227c59d0d1cab1b0c1cc21ac2131b9e9
andavi <andrew.bradley.davis@gmail.com>
2022-06-20 12:50:43 -0700

    ex 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,14 @@ import { useState } from 'react'
 
 const Statistics = (props) => {
   const { good, neutral, bad } = props
+  if (good + neutral + bad === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
   return (
     <>
       <h1>statistics</h1>
